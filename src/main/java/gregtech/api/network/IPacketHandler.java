93e54fbcb31bfa22f32f14b1ab430016958b41f7
DStrand1 <dane@strandboge.com>
2022-11-19 16:30:04 -0600

    clean up PacketHandler

--- src/main/java/gregtech/api/network/IPacketHandler.java
+++ src/main/java/gregtech/api/network/IPacketHandler.java
@@ -1,8 +1,5 @@
 package gregtech.api.network;
 
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
 public interface IPacketHandler {
 
     void registerPacket(Class<? extends IPacket> packetClass);
@@ -10,14 +7,4 @@ public interface IPacketHandler {
     int getPacketId(Class<? extends IPacket> packetClass);
 
     Class<? extends IPacket> getPacketClass(int packetId);
-
-    void registerServerExecutor(Class<? extends IServerExecutor> packetClass);
-
-    boolean hasServerExecutor(Class<? extends IServerExecutor> packetClass);
-
-    @SideOnly(Side.CLIENT)
-    void registerClientExecutor(Class<? extends IClientExecutor> packetClass);
-
-    @SideOnly(Side.CLIENT)
-    boolean hasClientExecutor(Class<? extends IClientExecutor> packetClass);
 }
--- src/main/java/gregtech/core/network/PacketHandler.java
+++ src/main/java/gregtech/core/network/PacketHandler.java
@@ -1,16 +1,8 @@
 package gregtech.core.network;
 
-import gregtech.api.network.IClientExecutor;
 import gregtech.api.network.IPacket;
 import gregtech.api.network.IPacketHandler;
-import gregtech.api.network.IServerExecutor;
 import net.minecraft.util.IntIdentityHashBiMap;
-import net.minecraftforge.fml.common.FMLCommonHandler;
-import net.minecraftforge.fml.relauncher.Side;
-import net.minecraftforge.fml.relauncher.SideOnly;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class PacketHandler implements IPacketHandler {
 
@@ -18,16 +10,8 @@ public class PacketHandler implements IPacketHandler {
 
     private final IntIdentityHashBiMap<Class<? extends IPacket>> packetMap;
 
-    @SideOnly(Side.CLIENT)
-    private List<Class<? extends IClientExecutor>> clientExecutors;
-    private final List<Class<? extends IServerExecutor>> serverExecutors;
-
     private PacketHandler(int initialCapacity) {
         packetMap = new IntIdentityHashBiMap<>(initialCapacity);
-        serverExecutors = new ArrayList<>();
-        if (FMLCommonHandler.instance().getEffectiveSide() == Side.CLIENT) {
-            clientExecutors = new ArrayList<>();
-        }
     }
 
     public static IPacketHandler getInstance() {
@@ -35,33 +19,18 @@ public class PacketHandler implements IPacketHandler {
     }
 
     private int ID = 1;
+    @Override
     public void registerPacket(Class<? extends IPacket> packetClass) {
         packetMap.put(packetClass, ID++);
     }
 
+    @Override
     public int getPacketId(Class<? extends IPacket> packetClass) {
         return packetMap.getId(packetClass);
     }
 
+    @Override
     public Class<? extends IPacket> getPacketClass(int packetId) {
         return packetMap.get(packetId);
-    }
-
-    public void registerServerExecutor(Class<? extends IServerExecutor> packetClass) {
-        serverExecutors.add(packetClass);
-    }
-
-    public boolean hasServerExecutor(Class<? extends IServerExecutor> packetClass) {
-        return serverExecutors.contains(packetClass);
-    }
-
-    @SideOnly(Side.CLIENT)
-    public void registerClientExecutor(Class<? extends IClientExecutor> packetClass) {
-        clientExecutors.add(packetClass);
-    }
-
-    @SideOnly(Side.CLIENT)
-    public boolean hasClientExecutor(Class<? extends IClientExecutor> packetClass) {
-        return clientExecutors.contains(packetClass);
     }
 }
